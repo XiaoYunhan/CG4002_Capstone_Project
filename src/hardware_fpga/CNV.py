f4b4a4ee28aa7bae482d186d81c438a3c1d71fac
Weng Kexin <42372486+Weng-Kexin@users.noreply.github.com>
2020-10-14 22:52:30 +0800

    Update CNV.py

--- src/hardware_fpga/CNV.py
+++ src/hardware_fpga/CNV.py
@@ -188,6 +188,21 @@ model.save(build_dir + "/cnv_synth.onnx")
 
 # In[ ]:
 
+# 5. Deployment
+import os
+from finn.transformation.fpgadataflow.make_deployment import DeployToPYNQ
 
+build_dir = "/workspace/finn/fpga"
+
+# FINN will use ssh to deploy and run the generated accelerator
+ip = os.getenv("pynq", "137.132.86.230")
+username = os.getenv("PYNQ_USERNAME", "xilinx")
+password = os.getenv("PYNQ_PASSWORD", "xilinx")
+port = os.getenv("PYNQ_PORT", 22)
+target_dir = os.getenv("PYNQ_TARGET_DIR", "/home/xilinx/finn_cnv")
+
+model = ModelWrapper(build_dir + "/cnv_synth.onnx")
+model = model.transform(DeployToPYNQ(ip, port, username, password, target_dir))
+model.save(build_dir + "/cnv_deploy.onnx")
 
 
