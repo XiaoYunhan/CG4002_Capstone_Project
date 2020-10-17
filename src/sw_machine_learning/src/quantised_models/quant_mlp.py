5a404e57d50db7b14920cf9a9d1a6af78873c590
Karan Sarat <karansarat@ymail.com>
2020-10-17 13:43:24 +0800

    Update quant_mlp params

--- src/sw_machine_learning/src/quantised_models/quant_mlp.py
+++ src/sw_machine_learning/src/quantised_models/quant_mlp.py
@@ -7,10 +7,10 @@ from brevitas.core.quant import QuantType
 class QuantMLP(nn.Module):
     def __init__(self, num_feature, num_class):
         super(QuantMLP, self).__init__()
-        self.layer_1 = qnn.QuantLinear(num_feature, 256, weight_quant_type=QuantType.INT, weight_bit_width=8)
-        self.layer_2 = qnn.QuantLinear(256, 128, weight_quant_type=QuantType.INT, weight_bit_width=8)
-        self.layer_3 = qnn.QuantLinear(128, 64, weight_quant_type=QuantType.INT, weight_bit_width=8)
-        self.layer_out = qnn.QuantLinear(64, num_class, weight_quant_type=QuantType.INT, weight_bit_width=8)
+        self.layer_1 = qnn.QuantLinear(num_feature, 256, bias=False, weight_quant_type=QuantType.INT, weight_bit_width=8)
+        self.layer_2 = qnn.QuantLinear(256, 128, bias=False, weight_quant_type=QuantType.INT, weight_bit_width=8)
+        self.layer_3 = qnn.QuantLinear(128, 64, bias=False, weight_quant_type=QuantType.INT, weight_bit_width=8)
+        self.layer_out = qnn.QuantLinear(64, num_class, bias=False, weight_quant_type=QuantType.INT, weight_bit_width=8)
         
         self.relu = qnn.QuantReLU(quant_type=QuantType.INT, bit_width=8, max_val=6)
         self.dropout = nn.Dropout(p=0.2)
