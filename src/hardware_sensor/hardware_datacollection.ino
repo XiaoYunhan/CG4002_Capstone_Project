16d669e1cb71c4cf08352de28ad5d935fa48dce6
tristyxxnana <35728638+tristyxxnana@users.noreply.github.com>
2020-10-21 13:42:47 +0800

    Update hardware_datacollection.ino

--- src/hardware_sensor/hardware_datacollection.ino
+++ src/hardware_sensor/hardware_datacollection.ino
@@ -4,30 +4,39 @@
 int count = 0;
 
 //store raw data of accel and gyro
-long accelX, accelY, accelZ;  
-long gyroX, gyroY, gyroZ;
+long accelXW, accelYW, accelZW;  
+long gyroXW, gyroYW, gyroZW;
+
+long accelXA, accelYA, accelZA;  
+long gyroXA, gyroYA, gyroZA;
 
 //calculate accel and gyro in degree and g
-float gForceX, gForceY, gForceZ;
-float rotX, rotY, rotZ;
+float gForceXW, gForceYW, gForceZW;
+float rotXW, rotYW, rotZW;
+
+float gForceXA, gForceYA, gForceZA;
+float rotXA, rotYA, rotZA;
 
 unsigned long currentMillis, previousMillis;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin(); //initialise I2C communication
-  setupMPU();
+  setupMPUW();
+  setupMPUA();
 }
 
 void loop() {
-  recordAccelRegisters(); //pre-processing
-  recordGyroRegisters();
+  recordAccelRegistersW(); //pre-processing
+  recordGyroRegistersW();
+  recordAccelRegistersA(); //pre-processing
+  recordGyroRegistersA();
   printData();
   delay(100);
 }
 
 //Establish communication with MPU and set up needed registers to read data from
-void setupMPU() {
+void setupMPUW() {
   Wire.beginTransmission(0b1101000); //I2C address of MPU
   Wire.write(0x6B); //Power Management
   Wire.write(0b00000000); //Power on MPU6050
@@ -46,45 +55,104 @@ void setupMPU() {
   Wire.endTransmission();
 }
 
+void setupMPUA() {
+  Wire.beginTransmission(0b1101001); //I2C address of MPU
+  Wire.write(0x6B); //Power Management
+  Wire.write(0b00000000); //Power on MPU6050
+  Wire.endTransmission();
+
+  //Gyro configuration. 
+  Wire.beginTransmission(0b1101001);
+  Wire.write(0x1B); 
+  Wire.write(0x00000000); //Set gyro full scale to +/- 250deg/sec
+  Wire.endTransmission();
+  
+  //Acc configuration. 
+  Wire.beginTransmission(0b1101001); 
+  Wire.write(0x1C); 
+  Wire.write(0b00000000); //Set acc full scale range to +/- 2g
+  Wire.endTransmission();
+}
+
 //retrieving raw data
-void recordAccelRegisters() {
+void recordAccelRegistersW() {
   Wire.beginTransmission(0b1101000); //I2C address of MPU6050
   Wire.write(0x3B); //Starting register for acc readings.
   Wire.endTransmission();
   Wire.requestFrom(0b1101000,6);  //Request 6 acc registers (3B-40)
 
   while(Wire.available() < 6);
-  accelX = Wire.read()<<8|Wire.read();  //Store first two bytes 
-  accelY = Wire.read()<<8|Wire.read();  //Store middle two bytes
-  accelZ = Wire.read()<<8|Wire.read();  //Store last two bytes
-  processAccelData();
+  accelXW = Wire.read()<<8|Wire.read();  //Store first two bytes 
+  accelYW = Wire.read()<<8|Wire.read();  //Store middle two bytes
+  accelZW = Wire.read()<<8|Wire.read();  //Store last two bytes
+  processAccelDataW();
 }
 
-void processAccelData() {
+void processAccelDataW() {
   //convert data to g. LSB per g = 16384.0
-  gForceX = accelX / 16384.0;
-  gForceY = accelY / 16384.0;
-  gForceZ = accelZ / 16384.0;
+  gForceXW = accelXW / 16384.0;
+  gForceYW = accelYW / 16384.0;
+  gForceZW = accelZW / 16384.0;
 }
 
-void recordGyroRegisters() {
+void recordAccelRegistersA() {
+  Wire.beginTransmission(0b1101001); //I2C address of MPU6050
+  Wire.write(0x3B); //Starting register for acc readings.
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101001,6);  //Request 6 acc registers (3B-40)
+
+  while(Wire.available() < 6);
+  accelXA = Wire.read()<<8|Wire.read();  //Store first two bytes 
+  accelYA = Wire.read()<<8|Wire.read();  //Store middle two bytes
+  accelZA = Wire.read()<<8|Wire.read();  //Store last two bytes
+  processAccelDataA();
+}
+
+void processAccelDataA() {
+  //convert data to g. LSB per g = 16384.0
+  gForceXA = accelXA / 16384.0;
+  gForceYA = accelYA / 16384.0;
+  gForceZA = accelZA / 16384.0;
+}
+
+void recordGyroRegistersW() {
   Wire.beginTransmission(0b1101000); //I2C address of MPU6050
   Wire.write(0x43); //Starting address for gyro readings 
   Wire.endTransmission();
   Wire.requestFrom(0b1101000,6);  //Request 6 gyro registers (43-48)
 
   while(Wire.available() < 6);
-  gyroX = Wire.read()<<8|Wire.read();  //Store first two bytes into
-  gyroY = Wire.read()<<8|Wire.read();  //Store middle two bytes into
-  gyroZ = Wire.read()<<8|Wire.read();  //Store last two bytes into
-  processGyroData();
+  gyroXW = Wire.read()<<8|Wire.read();  //Store first two bytes into
+  gyroYW = Wire.read()<<8|Wire.read();  //Store middle two bytes into
+  gyroZW = Wire.read()<<8|Wire.read();  //Store last two bytes into
+  processGyroDataW();
 }
 
-void processGyroData() {
+void processGyroDataW() {
   //convert data to degrees. LSB per degrees per second = 131.0;
-  rotX = gyroX / 131.0;
-  rotY = gyroY / 131.0;
-  rotZ = gyroZ / 131.0;
+  rotXW = gyroXW / 131.0;
+  rotYW = gyroYW / 131.0;
+  rotZW = gyroZW / 131.0;
+}
+
+void recordGyroRegistersA() {
+  Wire.beginTransmission(0b1101001); //I2C address of MPU6050
+  Wire.write(0x43); //Starting address for gyro readings 
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101001,6);  //Request 6 gyro registers (43-48)
+
+  while(Wire.available() < 6);
+  gyroXA = Wire.read()<<8|Wire.read();  //Store first two bytes into
+  gyroYA = Wire.read()<<8|Wire.read();  //Store middle two bytes into
+  gyroZA = Wire.read()<<8|Wire.read();  //Store last two bytes into
+  processGyroDataA();
+}
+
+void processGyroDataA() {
+  //convert data to degrees. LSB per degrees per second = 131.0;
+  rotXA = gyroXA / 131.0;
+  rotYA = gyroYA / 131.0;
+  rotZA = gyroZA / 131.0;
 }
 
 //Gyro x,y,z & Acc x,y,z
@@ -92,15 +160,27 @@ void printData() {
   count++;
   Serial.print(count);
   Serial.print(" ");
-  Serial.print(rotX, 4);
+  Serial.print(rotXW, 4);
   Serial.print(" ");
-  Serial.print(rotY, 4);
+  Serial.print(rotYW, 4);
   Serial.print(" ");
-  Serial.print(rotZ, 4);
+  Serial.print(rotZW, 4);
   Serial.print(" ");
-  Serial.print(gForceX, 4);
+  Serial.print(gForceXW, 4);
   Serial.print(" ");
-  Serial.print(gForceY, 4);
+  Serial.print(gForceYW, 4);
   Serial.print(" ");
-  Serial.println(gForceZ, 4);
+  Serial.print(gForceZW, 4);
+  Serial.print(" ");
+  Serial.print(rotXA, 4);
+  Serial.print(" ");
+  Serial.print(rotYA, 4);
+  Serial.print(" ");
+  Serial.print(rotZA, 4);
+  Serial.print(" ");
+  Serial.print(gForceXA, 4);
+  Serial.print(" ");
+  Serial.print(gForceYA, 4);
+  Serial.print(" ");
+  Serial.println(gForceZA, 4);
 }
