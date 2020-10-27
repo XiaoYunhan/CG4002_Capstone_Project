a930e8e73f421a63a893e22c262dfec32e4e6c94
Karan Sarat <karansarat@ymail.com>
2020-10-27 20:33:22 +0800

    Parallelised threads for multi user run

--- src/sw_machine_learning/multi_user_run.py
+++ src/sw_machine_learning/multi_user_run.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from queue import Queue
+from joblib import load
 from concurrent.futures import ThreadPoolExecutor
 
 import os
@@ -16,74 +16,99 @@ from src.models.mlp import *
 from sklearn.ensemble import RandomForestRegressor
 from comm_external.multiple_server import *
 
-IP_ADDRESS_1 = "127.0.0.1"
-PORT_1 = 8080
-GROUP_1 = 7
+IP_ADDRESS = ["127.0.0.1", "127.0.0.1", "127.0.0.1"]
+PORT= [8080, 8081, 8082]
+GROUP= [7, 7, 7]
 
-IP_ADDRESS_2 = "127.0.0.1"
-PORT_2 = 8081
-GROUP_2 = 7
-
-IP_ADDRESS_2 = "127.0.0.1"
-PORT_2 = 8082
-GROUP_2 = 7
-
-IDLE_FRAME = "-1/-1/-1/-1/-1/-1/-1/-1/-1/-1/-1/-1"
+IDLE_FRAME = "-1/-1/-1/-1/-1/-1"
 IGNORE_FRAME = 10
 
+class MultiUser():
+    def __init__(self):
+        self.model = load_model(os.getcwd() + "/models/")
+        self.model.eval()
+        self.rf = load('models/rf.joblib')
+        self.lock_model = threading.Lock()
+        self.lock_rf = threading.Lock()
 
-def init_server():
-    my_server = Server(IP_ADDRESS, PORT, GROUP)
-    my_server.start()
-    print("Server Started")
-    return my_server
+    def init_server(self, IP_ADDR, PRT, GRP):
+        my_server = Server(IP_ADDR, PRT, GRP)
+        my_server.start()
+        print("Server started at", IP_ADDRESS, PORT)
+        return my_server
 
 
-def load_model(PATH):
-    model = ffnn()
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.load_state_dict(torch.load(PATH + "mlp_1510_0451.pt", map_location=device))
-    print("Model Loaded")
-    return model
+    def load_model(self, PATH):
+        model = ffnn()
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        model.load_state_dict(torch.load(PATH + "mlp_1510_0451.pt", map_location=device))
+        print("Model Loaded")
+        return model
 
 
-def eval_model(model, frame):
-        Y_test_pred = model(frame)
-        Y_pred_softmax = torch.log_softmax(Y_test_pred, dim = 1)
-        _, Y_pred_tags = torch.max(Y_pred_softmax, dim = 1)
-        return Y_pred_tags.cpu().numpy()
+    def eval_model(self, model, frame):
+            Y_test_pred = model(frame)
+            Y_pred_softmax = torch.log_softmax(Y_test_pred, dim = 1)
+            _, Y_pred_tags = torch.max(Y_pred_softmax, dim = 1)
+            return Y_pred_tags.cpu().numpy()
 
-def process_data(server):  
-    prev_msg = ""
-    idle_count = 0
-    frame = []
-    while(1):
-        #print(server.raw_data)
-        move_data = '/'.join(server.raw.split("/")[1:7])
-        if prev_msg != move_data:
-            if move_data == IDLE_FRAME:
-                idle_count = idle_count + 1
-                if idle_count >= IGNORE_FRAME:
+    def process_data(self, server):  
+        prev_msg = ""
+        idle_count = 0
+        TIMEOUT = 0
+        move_frame = []
+        pos_frame = []
+        while 1:
+            move_data = '/'.join(server.raw.split("/")[1:7])
+            pos_data = '/'.join(server.raw.split("/")[7:13])
+            if prev_msg != move_data:
+                if TIMEOUT > 0:
+                    TIMEOUT = TIMEOUT - 1
+                if move_data == IDLE_FRAME:
+                    idle_count = idle_count + 1
+                    if idle_count >= IGNORE_FRAME:
+                        idle_count = 0
+                        move_frame.clear()
+                else:
+                    prev_msg = move_data
                     idle_count = 0
-                    frame.clear()
-            else:
-                prev_msg = move_data
-                idle_count = 0
-                frame = frame + move_data.split("/")
-                if len(frame) == 60:
-                    df = torch.from_numpy(np.array(MinMaxScaler().fit_transform([frame]))).float()
-                    out = eval_model(model, df)[0]
-                    #connect(datetime.now().strftime("%d-%m-%y"), ACTIONS[out], 0, 0, 0, 0, 0, 0, 0, 0)
-                    print("Predicted Dance Move: " + ACTIONS[out])
-                    del frame[0:12]
+                    move_frame = move_frame + move_data.split("/")
+                    if len(move_frame) == 60:
+                        df = torch.from_numpy(np.array(MinMaxScaler().fit_transform([move_frame]))).float()
+                        self.lock_model.acquire()
+                        try:
+                            out = eval_model(self.model, df)[0]
+                            #connect(datetime.now().strftime("%d-%m-%y"), ACTIONS[out], 0, 0, 0, 0, 0, 0, 0, 0)
+                            print("Predicted Dance Move: " + ACTIONS[out])
+                        finally:
+                            self.lock_model.release()
+                        del move_frame[0:12]
+                
+                if pos_data == IDLE_FRAME:
+                    pos_frame.clear()
+                else:
+                    pos_frame = pos_frame + pos_data.split("/")
+                    if len(pos_data) == 30:
+                        self.lock_rf.acquire()
+                        try:
+                            pos_out = self.rf.predict(np.array(pos_frame))
+                            if TIMEOUT == 0:
+                                if pos_out == 0:
+                                    print("Movement LEFT")
+                                else:
+                                    print("Movement RIGHT")
+                            TIMEOUT = 10
+                        finally:
+                            self.lock_rf.release()                          
+                            
 
-model = load_model(os.getcwd() + "/models/")
-model.eval()
+
 
 if __name__ == "__main__":
     s_list = []
+    mu = MultiUser()
     for i in range(3):
-        s_list.append(init_server())
+        s_list.append(mu.init_server(IP_ADDRESS[i], PORT[i], GROUP[i]))
 
     with ThreadPoolExecutor(max_workers=3) as executor:
-        executor.map(process_data, s_list)
+        executor.map(mu.process_data, s_list)
