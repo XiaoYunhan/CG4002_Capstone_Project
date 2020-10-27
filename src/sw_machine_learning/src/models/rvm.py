44a2803cc820f6b22daf33d73eed73416782e4cd
Karan Sarat <karansarat@ymail.com>
2020-10-27 16:50:25 +0800

    Change rvm input

--- src/sw_machine_learning/src/models/rvm.py
+++ src/sw_machine_learning/src/models/rvm.py
@@ -3,7 +3,8 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split 
 
 import pandas as pd
-import numpy, os
+import numpy as np
+import os
 
 def import_data(data_path, DATA_LEN=60):
     cols = []
@@ -14,6 +15,8 @@ def import_data(data_path, DATA_LEN=60):
     X = df.loc[:, :DATA_LEN-1]
     Y = df.loc[:, DATA_LEN:]
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=69)
+    X_train, Y_train = np.array(X_train), np.array(Y_train)
+    X_test, Y_test = np.array(X_test), np.array(Y_test)
     
     print("Data Loaded from File")
     return X_train, X_test, Y_train, Y_test
@@ -25,7 +28,7 @@ if __name__ == "__main__":
     # X = numpy.random.uniform(-1, 1, (240, 6))
     # X = numpy.round(X, 4)
     # Y = numpy.random.randint(2, size=(240, 1))
-    X_train, X_test, Y_train, Y_test = import_data(os.getcwd() + '/../../../data/Raw Data/positions/compiled_pos.txt')
+    X_train, X_test, Y_train, Y_test = import_data(os.getcwd() + '/../../../../data/Raw Data/positions/compiled_pos.txt')
 
     clf.fit(X_train, Y_train)
     y_predict = clf.predict(X_test)
