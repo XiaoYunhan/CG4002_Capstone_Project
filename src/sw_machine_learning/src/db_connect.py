64209d103d1ed3eec090ab444e49ec75a486c008
Karan Sarat <karansarat@ymail.com>
2020-11-05 03:27:52 +0800

    Troubleshooting queue transfer issues

--- src/sw_machine_learning/src/db_connect.py
+++ src/sw_machine_learning/src/db_connect.py
@@ -1,26 +1,34 @@
 import psycopg2
+import logging
 
 from comm_external.multiple_server import ACTIONS
 
-RDS_HOSTNAME = "localhost"
-RDS_USERNAME = "b07admin"
-RDS_PASSWORD = "password"
-RDS_DATABASE = "justdance"
-RDS_PORT = 3306
-flag = False
-date = "2020-10-27"
-dance_move = "0"
-left_time = "21:03:30.204"
-left_dancer = "0"
-center_time = "21:03:45.304"
-center_dancer = "0"
-right_time = "21:03:45.304"
-right_dancer = "0"
-diff_in_timing = "0"
-sync = "Yes"
+logging.basicConfig(level=logging.INFO,
+                      format='[%(levelname)s] (%(threadName)-9s) %(message)s',)
 
-def db_connect(self, queue):
-    
+def db_connect(queue):
+
+    RDS_HOSTNAME = "localhost"
+    RDS_USERNAME = "b07admin"
+    RDS_PASSWORD = "password"
+    RDS_DATABASE = "justdance"
+    RDS_PORT = 3306
+    is_pos = False
+    to_commit = False
+    date = "2020-11-03"
+    dance_move = "0"
+    left_time = "21:03:30.204"
+    left_dancer = "0"
+    center_time = "21:03:45.304"
+    center_dancer = "0"
+    right_time = "21:03:45.304"
+    right_dancer = "0"
+    diff_in_timing = "0"
+    sync = "Yes"
+    insertDanceDataQuery = "NAN"
+
+#def db_connect(queue):
+#    is_pos = False   
 
     connection = psycopg2.connect(user = RDS_USERNAME,
                                 password = RDS_PASSWORD,
@@ -29,28 +37,36 @@ def db_connect(self, queue):
                                 database = RDS_DATABASE)
     connection.autocommit = True
     cursor = connection.cursor()
-    print ( connection.get_dsn_parameters(),"\n")
+    logging.info ( connection.get_dsn_parameters())
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
+    #conn_msg = "You are connected to - " + record + "\n"
+    #logging.info(conn_msg)
 
     while True:
         if not queue.empty():
             cmd = queue.get()
+            logging.info(cmd)
             if cmd == "Start":
                 insertDanceDataQuery = "INSERT INTO dancedata VALUES (" + "'" + date + "'," +"'"+ cmd +"'"+ "," +"'"+left_time +"'"+ "," + "1" + "," +"'"+ center_time +"'"+ "," + "2" + "," +"'"+ right_time +"'"+ "," + "3" + "," + diff_in_timing + "," + "'" + sync + "')"
+                to_commit = True
             else:
                 for i in range(3):
                     if cmd[i][0] != -1:
-                        if cmd[i][0] == 8:
-                            insertDanceDataQuery = "INSERT INTO dancedata VALUES (" + "'" + date + "'," +"'"+ "End" +"'"+ "," +"'"+left_time +"'"+ "," + str(cmd[0][1]) + "," +"'"+ center_time +"'"+ "," + str(cmd[1][1]) + "," +"'"+ right_time +"'"+ "," + str(cmd[2][1]) + "," + diff_in_timing + "," + "'" + sync + "')"
-                        else:
-                            insertDanceDataQuery = "INSERT INTO dancedata VALUES (" + "'" + date + "'," +"'"+ ACTIONS[cmd[i][0]] +"'"+ "," +"'"+left_time +"'"+ "," + str(cmd[0][1]) + "," +"'"+ center_time +"'"+ "," + str(cmd[1][1]) + "," +"'"+ right_time +"'"+ "," + str(cmd[2][1]) + "," + diff_in_timing + "," + "'" + sync + "')"
-                        flag = True
+                        #if cmd[i][0] == 8:
+                         #   insertDanceDataQuery = "INSERT INTO dancedata VALUES (" + "'" + date + "'," +"'"+ "End" +"'"+ "," +"'"+left_time +"'"+ "," + str(cmd[0][1]) + "," +"'"+ center_time +"'"+ "," + str(cmd[1][1]) + "," +"'"+ right_time +"'"+ "," + str(cmd[2][1]) + "," + diff_in_timing + "," + "'" + sync + "')"
+                         #   to_commit = True
+                        #else:
+                        insertDanceDataQuery = "INSERT INTO dancedata VALUES (" + "'" + date + "'," +"'"+ ACTIONS[cmd[i][0]] +"'"+ "," +"'"+left_time +"'"+ "," + str(cmd[0][1]) + "," +"'"+ center_time +"'"+ "," + str(cmd[1][1]) + "," +"'"+ right_time +"'"+ "," + str(cmd[2][1]) + "," + diff_in_timing + "," + "'" + sync + "')"
+                        is_pos = True
+                        to_commit = True
                         break
-                if not flag:
+                if is_pos:
                     insertDanceDataQuery = "INSERT INTO dancedata VALUES (" + "'" + date + "'," +"'"+ str(cmd[0][0]) +"'"+ "," +"'"+left_time +"'"+ "," + str(cmd[0][1]) + "," +"'"+ center_time +"'"+ "," + str(cmd[0][1]) + "," +"'"+ center_time +"'"+ "," + str(cmd[1][1]) + "," +"'"+ right_time +"'"+ "," + str(cmd[2][1]) + "," + diff_in_timing + "," + "'" + sync + "')"
-                    flag = False
-            cursor.execute(insertDanceDataQuery)
+                    is_pos = False
+                    to_commit = True
+            if to_commit:
+                cursor.execute(insertDanceDataQuery)
+                to_commit = False
 
     
--- src/sw_machine_learning/src/multiuser.py
+++ src/sw_machine_learning/src/multiuser.py
@@ -2,6 +2,7 @@ from threading import Thread, Lock
 from queue import Queue
 from joblib import load
 import logging
+#from string import rstrip
 
 from sklearn.svm import SVC
 
@@ -9,13 +10,13 @@ from sklearn.preprocessing import MinMaxScaler
 
 #from src.models.mlp import *
 from sklearn.ensemble import RandomForestRegressor
-from ..comm_external.multiple_server import *
-from ..hardware_fpga.fpga import FinnDriver
+from comm_external.multiple_server import *
+#from hardware_fpga.fpga import FinnDriver
 
 IDLE_FRAME = "-1/-1/-1/-1/-1/-1"
 IGNORE_FRAME = 10
-START = 100
-END = 100
+START = 0
+END = 10000
 
 logging.basicConfig(level=logging.INFO,
                       format='[%(levelname)s] (%(threadName)-9s) %(message)s',)
@@ -24,14 +25,15 @@ class MultiUser():
     def __init__(self, queue, use_fpga):
         #self.model = load_model(os.getcwd() + "/models/")
         #self.model.eval()
+        logging.info("Multi User has been init")
         self.use_fpga = use_fpga
         if use_fpga:
-            self.driver = FinnDriver()
+            #self.driver = FinnDriver()
+            pass
         else:
             self.model = load('models/rbf.joblib')
         self.rf = load('models/rf.joblib')
-        self.lock_move = Lock()
-        self.lock_pos = Lock()
+        self.lock = Lock()
         self.queue = queue
         self.q_pkt = [(-1,1),(-1,2),(-1,3)]
 
@@ -57,75 +59,86 @@ class MultiUser():
     #     _, Y_pred_tags = torch.max(Y_pred_softmax, dim = 1)
     #     return Y_pred_tags.cpu().numpy()
 
-    def process_data(self, server, use_fpga):  
+    def process_data(self, server):  
         prev_msg = ""
         idle_count = 0
         MOVE_TIMEOUT = 0
         POS_TIMEOUT = 0
         move_frame = []
         pos_frame = []
-        start = False
+        start = True
         start_count = START 
         end_count = END
         out = -1
+        #q_pkt = [(-1,1),(-1,2),(-1,3)]
         while True:
-            if not start and start_count == 0:
-                start = True
-                self.queue.put("Start")
-            if start and end_count == 0:
-                self.q_pkt[server.id - 1] = (8,server.pos)
-                self.queue.put(self.q_pkt)
-                break
+            #if not start and start_count == 0:
+             #   start = True
+              #  self.queue.put("Start")
+            #if start and end_count == 0:
+             #   self.q_pkt[server.id - 1] = (8,server.pos)
+              #  self.queue.put(self.q_pkt)
+               # break
             move_data = '/'.join(server.raw_data.split("/")[1:7])
-            pos_data = '/'.join(server.raw_data.split("/")[7:13])
-            if prev_msg != move_data:
+            pos_data = '/'.join(server.raw_data.split("/")[7:13]).rstrip()
+            #logging.info("MV: " + move_data + " POS: " + pos_data)
+            if prev_msg != move_data and len(move_data) > 0 and len(pos_data) > 0:
+                #logging.info("MV: " + move_data + " POS: " + pos_data)
                 if MOVE_TIMEOUT > 0:
                     MOVE_TIMEOUT = MOVE_TIMEOUT - 1
                 if POS_TIMEOUT > 0:
                     POS_TIMEOUT = POS_TIMEOUT - 1
                 if move_data == IDLE_FRAME:
-                    if not start:
-                        start_count = start_count - 1
-                        continue
+                    #logging.info("Wrist is IDLE")
+                    #if not start:
+                     #   start_count = start_count - 1
+                      #  continue
                     idle_count = idle_count + 1
-                    end_count = end_count - 1
+                    #end_count = end_count - 1
                     if idle_count >= IGNORE_FRAME:
                         idle_count = 0
                         move_frame.clear()
-                else:
+                if move_data != IDLE_FRAME and pos_data == IDLE_FRAME:
+                    #logging.info("Wrist is moving, not Feet")
                     if not start:
                         start_count = START
                         continue
                     prev_msg = move_data
                     idle_count = 0
-                    end_count = END
+                    #end_count = END
                     move_frame = move_frame + move_data.split("/")
+                    #logging.info("Length of move frame is " + str(len(move_frame)))
                     if len(move_frame) == 60:
                         #df = torch.from_numpy(np.array(MinMaxScaler().fit_transform([move_frame]))).float()
-                        self.lock_move.acquire()
+                        self.lock.acquire()
                         try:
                         #out = eval_model(self.model, df)[0]
                             if self.use_fpga:
-                                move_frame = np.array(move_frame, dtype="float32")
-                                out = driver.predict(move_frame)
+                                pass
+                                #move_frame = np.array(move_frame, dtype="float32")
+                                #out = driver.predict(move_frame)
                             else:
                                 out = self.model.predict([move_frame])[0]
-                            msg = "Predicted Dance Move: " + ACTIONS[out]
+                            msg = str(server.id) + "-: Predicted Dance Move: " + ACTIONS[out]
                             logging.info(msg)
-                            self.q_pkt[server.id - 1] = (out, server.pos) 
-                            self.queue.put(self.q_pkt)
+                            self.q_pkt[server.id - 1] = (out, server.pos)
+                            logging.info(self.q_pkt) 
+                            self.queue.put_nowait(self.q_pkt)
                             self.q_pkt[server.id - 1] = (-1, server.pos)
                         finally:
-                            self.lock_move.release()
-                        del move_frame[0:12]
-                        MOVE_TIMEOUT = 12
+                            self.lock.release()
+                        #del move_frame[0:12]
+                        move_frame.clear()
+                        #MOVE_TIMEOUT = 12
                 
                 if pos_data == IDLE_FRAME or POS_TIMEOUT > 0:
                     pos_frame.clear()
-                else:
+                if move_data != IDLE_FRAME and pos_data != IDLE_FRAME:
+                    #logging.info("Feet Movement Detected")
                     pos_frame = pos_frame + pos_data.split("/")
-                    if len(pos_data) == 30:
-                        self.lock_pos.acquire()
+                    if len(pos_frame) == 30:
+                        logging.info("Position change detected")
+                        self.lock.acquire()
                         try:
                             pos_out = np.round(np.clip(self.rf.predict([pos_frame]), 0, 1)).astype(bool)[0]
                             if pos_out:
@@ -138,7 +151,8 @@ class MultiUser():
                                     server.pos =  server.pos - 1
                             pos_frame.clear()
                             self.q_pkt[server.id - 1] = (-1, server.pos) 
+                            logging.info(self.q_pkt)
                             self.queue.put(self.q_pkt)
-                            POS_TIMEOUT = 10
+                            #POS_TIMEOUT = 10
                         finally:
-                            self.lock_pos.release()        
+                            self.lock.release()        
